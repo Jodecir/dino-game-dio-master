b6cbae37f6844dc7e80267f5dfefb75b9d6d01f2
Jodecir Neto <jodecirneto@gmail.com>
2020-12-08 12:11:52 -0300

    dino jump

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const dino = document.querySelector('.dino');
+
+let isJumping = false;
+let position = 0;
+
+console.log(dino)
+
+document.addEventListener('keyup', handleKeyUp);
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+    }
+  }
+}
+
+function jump() {
+  isJumping = true;
+
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      // Descendo
+      clearInterval(upInterval);
+
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20);
+    } else {
+      // Subindo
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+  }, 20);
+}
