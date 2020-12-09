47c8e60b235f069ca92d32557a9f1efcf61fb295
Jodecir Neto <jodecirneto@gmail.com>
2020-12-09 09:08:35 -0300

    cactus slider

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background-game');
 
 let isJumping = false;
 let position = 0;
-
-console.log(dino)
-
-document.addEventListener('keyup', handleKeyUp);
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
@@ -20,7 +17,6 @@ function jump() {
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
-      // Descendo
       clearInterval(upInterval);
 
       let downInterval = setInterval(() => {
@@ -33,9 +29,33 @@ function jump() {
         }
       }, 20);
     } else {
-      // Subindo
       position += 20;
       dino.style.bottom = position + 'px';
     }
   }, 20);
 }
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  cactus.classList.add('cactus');
+  cactus.style.left = cactusPosition + 'px';
+  background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime);
+}
+
+document.addEventListener('keyup', handleKeyUp);
+createCactus();
